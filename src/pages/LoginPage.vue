3568b54683a43b389eee0b9f0a3de895be5f16ca
Stephanos Massouras <s.massouras@outlook.com>
2023-03-30 17:40:30 +0300

    login page created

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="fixed-center">
+    <h4>VUestionnaire</h4>
+    <div class="q-pa-md">
+      <div class="q-gutter-md" style="max-width: 500px">
+        <q-input rounded outlined label="username" />
+        <q-input rounded outlined label="email" />
+        <q-input rounded outlined label="password" />
+      </div>
+      <div class="q-pa-md q-gutter-sm">
+        <q-btn color="blue" text-color="white" label="Login" />
+      </div>
+    </div>
+  </div>
+</template>

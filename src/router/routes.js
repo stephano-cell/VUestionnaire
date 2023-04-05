8f9d7a39e9286bfe42aa1071a2e9cc805776e48b
Stephanos Massouras <s.massouras@outlook.com>
2023-04-05 19:46:38 +0300

    arristos commit to be able to create users/edit on a pop up tab

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,17 +2,33 @@ const routes = [
   {
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
+    children: [
+      { path: "", component: () => import("pages/IndexPage.vue") },
+      {
+        path: "/admin",
+        component: () => import("pages/admin/ListUsers.vue"),
+      },
+      {
+        path: "/admin/new",
+        component: () => import("pages/admin/ViewUser.vue"),
+        props: () => ({
+          mode: "new",
+        }),
+      },
+      {
+        path: "/admin/edit",
+        component: () => import("pages/admin/ViewUser.vue"),
+        props: (route) => ({
+          mode: "edit",
+          id: route.params.id,
+        }),
+      },
+    ],
   },
-
   {
     path: "/login",
     name: "login",
     component: () => import("pages/LoginPage.vue"),
-  },
-  {
-    path: "/AdminUsers",
-    component: () => import("pages/AdminUsers.vue"),
   },
   // Always leave this as last one,
   // but you can also remove it

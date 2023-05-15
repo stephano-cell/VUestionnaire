3aebc5c407f274a2419736016b7e171e5d2f6a79
Stephanos Massouras <s.massouras@outlook.com>
2023-05-15 18:51:13 +0300

    adminUserList button redirects

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,7 +2,12 @@
   <q-page class="flex flex-center">
     <q-tabs>
       <q-tab name="users"
-        ><q-icon color="orange" name="face" size="20em" />
+        ><q-icon
+          color="orange"
+          name="face"
+          size="20em"
+          @click="adminUserList"
+        />
         Clients
       </q-tab>
       <q-tab name="projects"
@@ -19,9 +24,34 @@
 
 <script>
 import { defineComponent } from "vue";
+import { ref, toRefs } from "vue";
+import { useRouter } from "vue-router";
+import { useAppStore } from "../stores/appStore";
 
 export default defineComponent({
   name: "IndexPage",
+  setup(props) {
+    const { redirect } = toRefs(props);
+    console.log("Redirection: " + redirect?.value);
+    const username = ref("admin");
+    const password = ref("pass");
+    const router = useRouter();
+    const store = useAppStore();
+
+    const adminUserList = () => {
+      // Call authenticate with the value properties of username and password
+      if (store.authenticate(username.value, password.value)) {
+        console.log("Navigating");
+        router.replace("/admin/user/list");
+      }
+    };
+
+    return {
+      adminUserList,
+      username,
+      password,
+    };
+  },
 });
 </script>
 

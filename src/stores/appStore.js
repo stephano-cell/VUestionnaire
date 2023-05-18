96083fa67ebc8b29aa339a75f637fd9e54e46114
Stephanos Massouras <s.massouras@outlook.com>
2023-05-18 22:36:15 +0300

    import LocalStorage appStore.js ->persist login

--- src/stores/appStore.js
+++ src/stores/appStore.js
@@ -1,9 +1,10 @@
 import { defineStore } from "pinia";
+import { LocalStorage } from "quasar";
 
-// This stores the state of the entire app, spesific features use their own stores
+// This stores the state of the entire app, specific features use their own stores
 export const useAppStore = defineStore("appStore", {
   state: () => ({
-    auth: null,
+    auth: LocalStorage.getItem("auth") || null, // get 'auth' state from local storage
   }),
   getters: {
     authenticated() {
@@ -15,10 +16,12 @@ export const useAppStore = defineStore("appStore", {
     logout() {
       console.log("Logout");
       this.auth = null;
+      LocalStorage.remove("auth"); // remove 'auth' state from local storage
     },
     authenticate(username, pass) {
       if (username == "admin" && pass == "pass") {
         this.auth = "admin";
+        LocalStorage.set("auth", this.auth); // set 'auth' state to local storage
         console.log("Login as: " + this.auth);
         return true;
       }

035b4305572f24577cbb51e5583e5e20e0452d8d
Stephanos Massouras <s.massouras@outlook.com>
2023-05-30 10:10:59 +0300

    list wysiwyg

--- src/pages/admin/ListProjects.vue
+++ src/pages/admin/ListProjects.vue
@@ -43,6 +43,77 @@
                 min-height="5rem"
                 autofocus
                 @keyup.enter.stop
+                :dense="$q.screen.lt.md"
+                :toolbar="[
+                  ['bold', 'italic', 'strike', 'underline'],
+
+                  [
+                    {
+                      label: $q.lang.editor.formatting,
+                      icon: $q.iconSet.editor.formatting,
+                      list: 'no-icons',
+                      options: ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+                    },
+                    {
+                      label: $q.lang.editor.fontSize,
+                      icon: $q.iconSet.editor.fontSize,
+                      fixedLabel: true,
+                      fixedIcon: true,
+                      list: 'no-icons',
+                      options: [
+                        'size-1',
+                        'size-2',
+                        'size-3',
+                        'size-4',
+                        'size-5',
+                        'size-6',
+                        'size-7',
+                      ],
+                    },
+                    {
+                      label: $q.lang.editor.defaultFont,
+                      icon: $q.iconSet.editor.font,
+                      fixedIcon: true,
+                      list: 'no-icons',
+                      options: [
+                        'default_font',
+                        'arial',
+                        'arial_black',
+                        'comic_sans',
+                        'courier_new',
+                        'impact',
+                        'lucida_grande',
+                        'times_new_roman',
+                        'verdana',
+                      ],
+                    },
+                    'removeFormat',
+                  ],
+                  [
+                    {
+                      label: $q.lang.editor.align,
+                      icon: $q.iconSet.editor.align,
+                      fixedLabel: true,
+                      list: 'only-icons',
+                      options: ['left', 'center', 'right', 'justify'],
+                    },
+                    'unordered',
+                    'ordered',
+                  ],
+
+                  ['undo', 'redo'],
+                  ['fullscreen'],
+                ]"
+                :fonts="{
+                  arial: 'Arial',
+                  arial_black: 'Arial Black',
+                  comic_sans: 'Comic Sans MS',
+                  courier_new: 'Courier New',
+                  impact: 'Impact',
+                  lucida_grande: 'Lucida Grande',
+                  times_new_roman: 'Times New Roman',
+                  verdana: 'Verdana',
+                }"
               />
             </q-popup-edit>
           </q-td>
